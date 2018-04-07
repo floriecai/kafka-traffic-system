e7cf51b9bcfaff29bd7ac26bd3b0cf63e325efca
Florie Cai <floriecai@hotmail.com>
2018-04-06 18:05:33 -0700

    Server can now go offline and reconnect

--- node/clusterlib/readwrite.go
+++ node/clusterlib/readwrite.go
@@ -69,11 +69,14 @@ func MountFiles(path string) {
 
 	// First time a node has registered with a server
 	if _, err := os.Stat(fname); os.IsNotExist(err) {
-		_, err := os.Create(fname)
+		f, err := os.Create(fname)
 		if err != nil {
 			checkError(err, "MountFiles CreateFiles")
 			log.Fatalf("Couldn't create file [%s]", fname)
 		}
+
+		f.Sync()
+		defer f.Close()
 
 		return
 	}
